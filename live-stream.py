31893409ad8ba2d43700ea8c29b27584a06073db
Mahesh M <maheshm@Maheshs-MacBook-Air.local>
2023-03-25 20:53:12 -0400

    added graph generation

--- live-stream.py
+++ live-stream.py
@@ -4,19 +4,30 @@ import psutil
 import matplotlib.pyplot as plt
 from flask import Flask, render_template, Response, stream_with_context, request
 
-video = cv2.VideoCapture("video-light.mp4")
+video = cv2.VideoCapture("trim.mp4")
 app = Flask('__name__')
 
-
 def video_stream():
+    i = 0
     while True:
         ret, frame = video.read()
         if not ret:
-            break;
+            break
         else:
+            cpu_usage = psutil.cpu_percent()
+            mem_usage = psutil.virtual_memory().percent
+            # Creating the scatter plot
+            plt.scatter(i, cpu_usage, color = "red")
+            plt.scatter(i, mem_usage, color = "blue")
+            plt.legend(["CPU", "Memory"], loc ="lower right")
+            i+=1
+            # plt.pause(0.1)
+
             ret, buffer = cv2.imencode('.jpeg',frame)
             frame = buffer.tobytes()
             yield (b' --frame\r\n' b'Content-type: imgae/jpeg\r\n\r\n' + frame +b'\r\n')
+    plt.savefig('my_plot.png')
+    plt.close()
 
 
 @app.route('/live-stream')
